e554a79b581db0f251ea04f8a6666171939cfce3
Lauri Niva <niva.lauri@gmail.com>
2022-10-07 17:38:22 +0300

    Lisää Mantine notifikaatio provider

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { NotificationsProvider } from '@mantine/notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <NotificationsProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </NotificationsProvider>
   </Router>
 );
