f618a124b16d7721f5c74959cf3303cbbf2a1d11
Lauri Niva <niva.lauri@gmail.com>
2022-10-10 07:54:39 +0300

    Header ulkoasun muutoksia

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,22 +1,28 @@
-import { Button, Container, Group, Header, Loader, Menu, Title, UnstyledButton } from '@mantine/core';
-import { IconLogout, IconUserCircle } from '@tabler/icons';
+import {
+  Button,
+  Container,
+  Group,
+  Header,
+  Loader,
+  Menu,
+  Title,
+  UnstyledButton,
+  MediaQuery,
+} from '@mantine/core';
+import { IconLogout, IconMap2, IconUserCircle } from '@tabler/icons';
 import Avatar from 'boring-avatars';
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { signOut } from '../services/auth.service';
 
-
-
 function MainHeader() {
-
   const session = useSelector((state) => state.sessions);
   const username = useSelector((state) => state.users);
 
   const loggedinUser = session?.user;
 
   const MenuButton = () => {
-
     const handleSignout = async () => {
       try {
         await signOut();
@@ -34,6 +40,9 @@ function MainHeader() {
         </Menu.Target>
         <Menu.Dropdown>
           <Menu.Label>{username}</Menu.Label>
+          <Link className="top-menu-link" to="/own">
+            <Menu.Item icon={<IconUserCircle />}>Omat</Menu.Item>
+          </Link>
           <Link className="top-menu-link" to="/userprofile">
             <Menu.Item icon={<IconUserCircle />}>Profiili</Menu.Item>
           </Link>
@@ -45,42 +54,45 @@ function MainHeader() {
     );
   };
 
-   return (
-     <Header sx={{ backgroundColor: '#364FC7' }} height={50} p="0">
-       <Container
-         sx={{
-           display: 'flex',
-           justifyContent: 'space-between',
-           alignItems: 'center',
-           height: '100%',
-         }}
-       >
-         <Title color="teal.4" order={1}>
-           Kaupunki kuntoon
-         </Title>
-         <Group>
-           <Link to="/">
-             <Button color="teal.5">Kartta</Button>
-           </Link>
-           {loggedinUser ? (
-             <>
-               <Link to="/new">
-                 <Button color="teal.5">Uusi</Button>
-               </Link>
-               <Link to="/own">
-                 <Button color="teal.5">Omat</Button>
-               </Link>
-               <MenuButton />
-             </>
-           ) : (
-             <Link to="/login">
-               <Button>Login</Button>
-             </Link>
-           )}
-         </Group>
-       </Container>
-     </Header>
-   );
+  return (
+    <Header sx={{ backgroundColor: '#364FC7' }} height={50} p="0">
+      <Container
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          height: '100%',
+        }}
+      >
+        <Group>
+          <MediaQuery smallerThan="xs" styles={{ fontSize: '1.2rem' }}>
+            <Title color="teal.4" order={1}>
+              Kaupunki kuntoon
+            </Title>
+          </MediaQuery>
+        </Group>
+        <Group>
+          <Link to="/">
+            <Button color="teal.5">
+              <IconMap2 />
+            </Button>
+          </Link>
+          {loggedinUser ? (
+            <>
+              {/* <Link to="/new">
+                <Button color="teal.5"><IconCirclePlus /></Button>
+              </Link> */}
+              <MenuButton />
+            </>
+          ) : (
+            <Link to="/login">
+              <Button>Login</Button>
+            </Link>
+          )}
+        </Group>
+      </Container>
+    </Header>
+  );
 }
 
-export default MainHeader
+export default MainHeader;
