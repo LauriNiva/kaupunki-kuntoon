0fc21a773965c30bbb05ac6cd12c981397ed6ed9
Lauri Niva <niva.lauri@gmail.com>
2022-10-28 13:14:11 +0300

    Osaston päivitys raporttiin

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,50 +1,130 @@
 import {
   Badge,
+  Button,
   Center,
   CloseButton,
   Container,
+  Group,
   Image,
   Loader,
   Modal,
   Paper,
+  Select,
   Stepper,
   Text,
+  Title,
 } from '@mantine/core';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getSingleReport } from '../services/report.service';
+import { supabase } from '../supabaseClient';
 
 function Report() {
   const { id } = useParams();
-  const loggedinUserId = useSelector((state) => state.sessions?.user.id);
+  const user = useSelector((state) => state.users);
+  const loggedinUserId = user?.id;
+
+  console.log('user', user);
 
   const [imageFullscreen, setImageFullscreen] = useState(false);
+  const [departmentModalOpen, setDepartmentModalOpen] = useState(false);
 
   const [report, setReport] = useState(null);
-  const [reportStep, setReportStep] = useState(1);
+  // const [reportStep, setReportStep] = useState(3);
+  const reportStep = report?.status;
+
+  const [departments, setDepartments] = useState(null);
+
+  const fetchReport = async () => {
+    const fetchedReport = await getSingleReport(id);
+    setReport(fetchedReport);
+  };
 
   useEffect(() => {
-    const fetchReport = async () => {
-      const fetchedReport = await getSingleReport(id);
-      setReport(fetchedReport);
-    };
     fetchReport();
-  }, [id]);
+  }, []);
+
+  useEffect(() => {
+    const fetchDepartments = async () => {
+      const { data, error } = await supabase.from('departments').select();
+      if (error) console.log(error);
+      if (data) {
+        setDepartments(data);
+      }
+    };
+
+    if (user?.role === 'operator') {
+      fetchDepartments();
+    }
+  }, [user]);
 
   const isOwner = loggedinUserId === report?.user_id;
-  console.log(report);
+  console.log('---report:', report);
 
   const imageSrc = !report?.images
     ? 'https://yeopeoovpnhcjzmqilyz.supabase.co/storage/v1/object/public/kaupunki-images/default.jpg'
     : `https://yeopeoovpnhcjzmqilyz.supabase.co/storage/v1/object/public/kaupunki-images/${report?.images}`;
+
+  const updateReportDepartment = async (departmentid) => {
+    console.log(departmentid, report.id);
+    // const { data, error } = await supabase
+    //   .from('department_reports')
+    //   .upsert({ department: departmentid, report: report.id })
+    //   .select();
+
+    const { data, error } = await supabase
+      .from('reports')
+      .update({ department: departmentid, public: true, status: 2 })
+      .eq('id', report.id)
+      .select();
+
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      console.log(data);
+      fetchReport();
+      setDepartmentModalOpen(false);
+    }
+  };
+
+  const DepartmentModal = () => {
+    let selectedDepartment;
+
+    return (
+      <Modal
+        opened={departmentModalOpen}
+        onClose={() => setDepartmentModalOpen(false)}
+        centered
+      >
+        <Container>
+          <Text align="center">Ohjaa raportti oikealle osastolle.</Text>
+          <Text align="center">
+            Osaton valinta muuttaa raportin julkiseksi.
+          </Text>
+          <Select
+            my={'lg'}
+            onChange={(value) => (selectedDepartment = value)}
+            data={departments?.map((d) => ({ value: d.id, label: d.name }))}
+          />
+          <Group position="apart">
+            <Button>Peruuta</Button>
+            <Button onClick={() => updateReportDepartment(selectedDepartment)}>
+              Tallenna
+            </Button>
+          </Group>
+        </Container>
+      </Modal>
+    );
+  };
 
   return (
     <Center>
       {!report ? (
         <Loader size={'lg'} />
       ) : (
-        <Container>
+        <Container mt={'lg'} mb={'xl'}>
           <Modal
             withCloseButton={false}
             padding={0}
@@ -67,21 +147,65 @@ function Report() {
           />
 
           <Paper p="md" shadow="sm" withBorder mt="lg">
-            {isOwner && <Badge>Oma</Badge>}
-            <Text> {report.description}</Text>
+            <Title mb={'lg'} align="center" order={3}>
+              Raportti
+            </Title>
+            <Group position='center'>
+              {isOwner && <Badge>Oma</Badge>}
+              {report?.public && <Badge>Julkinen</Badge>}
+              {report?.status === 4 && <Badge>Valmis</Badge>}
+            </Group>
+            <Text align="center" my={'md'}>
+              {report.description}
+            </Text>
           </Paper>
+
           <Paper p="md" shadow="sm" withBorder mt="lg">
+            <Title mb={'lg'} align="center" order={3}>
+              Vastuuosasto
+            </Title>
+            <Text align="center">
+              {report.department
+                ? report.department
+                : 'Raporttia ei ole vielä ohjattu eteenpäin.'}
+            </Text>
+            {user?.role === 'operator' && (
+              <Button
+                onClick={() => setDepartmentModalOpen(true)}
+                fullWidth
+                color="cyan.4"
+                my={'md'}
+              >
+                {!report.department ? 'Ohjaa' : 'Vaihda osastoa'}
+              </Button>
+            )}
+            <DepartmentModal />
+          </Paper>
+
+          <Paper p="md" shadow="sm" withBorder mt="lg">
+            <Title mb={'lg'} align="center" order={3}>
+              Tila
+            </Title>
             <Stepper active={reportStep} breakpoint="xs">
+              <Stepper.Step label="Luotu" description="Luotu"></Stepper.Step>
               <Stepper.Step label="Luettu" description="Luettu">
-                Raportti luotu ja lähetetty. Odottaa lukemista.
+                <Text align="center">
+                  Raportti luotu ja lähetetty. Odottaa lukemista.
+                </Text>
               </Stepper.Step>
               <Stepper.Step label="Työn alla" description="Työn alla">
-                Ohjattu oikealle osastolle/henkilölle.
+                <Text align="center">
+                  Raportti luettu ja ohjattu eteenpäin siitä vastaavalle
+                  osastolle.
+                </Text>
               </Stepper.Step>
               <Stepper.Step label="Valmis" description="Valmis">
-                Valmis
+                <Text align="center">Raportti on otettu työn alle.</Text>
               </Stepper.Step>
-              <Stepper.Completed>Valmis.</Stepper.Completed>
+              <Stepper.Completed>
+                <Text align="center">Valmis.</Text>
+                <Text align="center">{report.comment}</Text>
+              </Stepper.Completed>
             </Stepper>
           </Paper>
         </Container>
