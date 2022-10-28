ca703e0ea37545826d63c9d0547ff23b61db7df6
Lauri Niva <niva.lauri@gmail.com>
2022-10-28 12:17:32 +0300

    Osasto mukaan yksittäistä raporttia hakiessa

--- src/services/report.service.js
+++ src/services/report.service.js
@@ -2,7 +2,10 @@ import { supabase } from '../supabaseClient';
 import { v4 as uuidv4 } from 'uuid';
 
 export const getPublicReports = async () => {
-  const { data, error } = await supabase.from('reports').select().eq('public', true);
+  const { data, error } = await supabase
+    .from('reports')
+    .select()
+    .eq('public', true);
 
   if (error) console.log(error);
 
@@ -10,10 +13,17 @@ export const getPublicReports = async () => {
 };
 
 export const getSingleReport = async (id) => {
-  const { data, error } = await supabase.from('reports').select().eq('id', id).single();
+  const { data, error } = await supabase
+    .from('reports')
+    .select('*, departments(name)')
+    .eq('id', id)
+    .single();
   if (error) console.log(error);
 
-  if (data) return data;
+  if (data) {
+    if (data.departments?.name) data.department = data.departments.name;
+    return data;
+  }
 };
 
 export const getOwnReports = async (userid) => {
@@ -37,9 +47,7 @@ export const getGroupReports = async (userid) => {
   if (data) return data;
 };
 export const getAllReports = async (userid) => {
-  const { data, error } = await supabase
-    .from('reports')
-    .select();
+  const { data, error } = await supabase.from('reports').select();
 
   if (error) console.log('getAllReports error:', error);
 
