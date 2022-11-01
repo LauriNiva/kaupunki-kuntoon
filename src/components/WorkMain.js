f2c7b8b7bdccaa9de7a289f52cce8be7bd981f00
Lauri Niva <niva.lauri@gmail.com>
2022-11-01 09:41:18 +0200

    Osastot

--- src/components/WorkMain.js
+++ src/components/WorkMain.js
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 function WorkMain() {
   const user = useSelector((state) => state.users);
   const reports = useSelector((state) => state.reports);
+  const departments = useSelector((state) => state.departments);
   console.log(reports);
 
   const navigate = useNavigate();
@@ -16,7 +17,7 @@ function WorkMain() {
   }, [user, navigate]);
 
   const newReports = () => {
-    return reports.map((report) => (   
+    return reports.department.map((report) => (
       <tr onClick={() => navigate(`/reports/${report.id}`)} key={report.id}>
         <td>
           <Tooltip label={new Date(report.created_at).toLocaleTimeString()}>
@@ -26,13 +27,13 @@ function WorkMain() {
           </Tooltip>
           {/* options jos halutaan kellonaika {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'} */}
         </td>
-        <td className='table-description-cell'>
-          <Container m={0} p={0} size='20vw'>
-          {report.description}
+        <td className="table-description-cell">
+          <Container m={0} p={0} size="20vw">
+            {report.description}
           </Container>
-          </td>
+        </td>
         <td>{report.status}</td>
-        <td>{report.department}</td>
+        <td>{departments[report.department]}</td>
         <td>{report.public ? <IconEye /> : <IconEyeOff />}</td>
       </tr>
     ));
@@ -40,13 +41,13 @@ function WorkMain() {
 
   return (
     <Container>
-      <Table sx={{ }} mt={'md'} highlightOnHover>
+      <Table sx={{}} mt={'md'} highlightOnHover>
         <thead>
           <tr>
             <th>Luotu</th>
             <th>Raportti</th>
-            <th>status</th>
-            <th>department</th>
+            <th>Tila</th>
+            <th>Osasto</th>
             <th>public</th>
           </tr>
         </thead>
