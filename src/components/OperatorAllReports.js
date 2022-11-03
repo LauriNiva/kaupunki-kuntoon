0f4719b6d0fecda419bbe375f3e711db4130b904
Lauri Niva <niva.lauri@gmail.com>
2022-11-03 09:03:52 +0200

    Selkeämpi lista raporteista

--- src/components/OperatorAllReports.js
+++ src/components/OperatorAllReports.js
@@ -1,4 +1,14 @@
-import { Container, Table, Text, Title, Tooltip } from '@mantine/core';
+import {
+  Container,
+  Divider,
+  Grid,
+  NativeSelect,
+  Paper,
+  Table,
+  Text,
+  Title,
+  Tooltip,
+} from '@mantine/core';
 import { IconEye, IconEyeOff } from '@tabler/icons';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -7,13 +17,13 @@ import { useNavigate } from 'react-router-dom';
 function OperatorAllReports() {
   const user = useSelector((state) => state.users);
   const reports = useSelector((state) => state.reports);
-  console.log(reports);
+  const departments = useSelector((state) => state.departments);
+
+  const [selectedSort, setSelectedSort] = useState('created_new');
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log('bleep');
-    console.log(user);
     if (user) {
       if (user.role !== 'operator') navigate('/');
     }
@@ -34,18 +44,81 @@ function OperatorAllReports() {
     }
   };
 
-  const reportsToShow = reports.all?.filter((report) => {
-    if (publicFilter === 1) {
-      return true;
-    } else if (publicFilter === 2) {
-      return report.public === true;
-    } else {
-      return report.public === false;
+  const reportsToShow = () => {
+    let reportsToReturn = [...reports?.all];
+      // TEHOKKAAMPAA TEHDÄ new Date raportteja hakiessa tai jopa palvelimella???
+    if (selectedSort === 'created_new') {
+      console.log('uusin')
+      reportsToReturn.sort((a, b) => new Date(b.created_at) - new Date(a.created_at) )
+    } else if (selectedSort === 'created_old') {
+      console.log('vanhin')
+      reportsToReturn.sort(
+        (a, b) => new Date(a.created_at) - new Date(b.created_at)
+      );
+    } else if (selectedSort === 'updated_new') {
+      reportsToReturn.sort(
+        (a, b) => new Date(b.updated_at) - new Date(a.updated_at)
+      );
+    } else if (selectedSort === 'updated_old') {
+       reportsToReturn.sort(
+         (a, b) => new Date(a.updated_at) - new Date(b.updated_at)
+       );
     }
-  });
+    console.log(reportsToReturn);
+
+    return reportsToReturn;
+  };
+
+  const SingleReport = ({ report }) => {
+    return (
+      <Paper withBorder shadow={'md'} mb={'md'} p={'md'}>
+        <Grid gutter={'xs'}>
+          <Grid.Col span={6}>
+            <Text>
+              {new Date(report.created_at).toLocaleDateString('fi-Fi')}
+            </Text>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <Text align="right">
+              {new Date(report.updated_at).toLocaleDateString('fi-Fi')}
+            </Text>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <Text>{departments[report.department]}</Text>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <Text align="right">{report.status}</Text>
+          </Grid.Col>
+          <Grid.Col span={12}>
+            <Divider />
+            <Text>{report.description}</Text>
+          </Grid.Col>
+        </Grid>
+      </Paper>
+    );
+  };
+
+  const FilterHeader = () => {
+    const sorts = [
+      { value: 'created_new', label: 'Uusin' },
+      { value: 'created_old', label: 'Vanhin' },
+      { value: 'updated_new', label: 'Viimeksi muokattu' },
+      { value: 'updated_old', label: 'Vanhin muokattu' },
+    ];
+    return (
+      <Paper withBorder shadow={'md'} my={'md'} p={'md'}>
+        <NativeSelect
+          value={selectedSort}
+          onChange={(e) => setSelectedSort(e.currentTarget.value)}
+          label="Järjestys"
+          data={sorts}
+        />
+      </Paper>
+    );
+  };
 
   const newReports = () => {
-    return reportsToShow.map((report) => (
+    return reportsToShow().map((report) => (
       <tr onClick={() => navigate(`/reports/${report.id}`)} key={report.id}>
         <td>
           <Tooltip label={new Date(report.created_at).toLocaleTimeString()}>
@@ -57,7 +130,7 @@ function OperatorAllReports() {
         </td>
         <td className="table-description-cell">{report.description}</td>
         <td>{report.status}</td>
-        <td>{report.department}</td>
+        <td>{departments[report.department]}</td>
         <td>{report.public ? <IconEye /> : <IconEyeOff />}</td>
       </tr>
     ));
@@ -68,7 +141,13 @@ function OperatorAllReports() {
       <Title align="center" order={2}>
         Kaikki raportit
       </Title>
-      <Table sx={{ tableLayout: 'fixed' }} mt={'md'} highlightOnHover>
+      <FilterHeader />
+      <Container>
+        {reportsToShow().map((report) => (
+          <SingleReport report={report} key={report.id} />
+        ))}
+      </Container>
+      {/* <Table sx={{ tableLayout: 'fixed' }} mt={'md'} highlightOnHover>
         <thead>
           <tr>
             <th style={{ width: '4rem' }}>Luotu</th>
@@ -84,7 +163,7 @@ function OperatorAllReports() {
           </tr>
         </thead>
         <tbody>{reports.all && newReports()}</tbody>
-      </Table>
+      </Table> */}
     </Container>
   );
 }
