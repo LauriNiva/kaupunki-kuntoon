c7a336655f35c909c52d0484d34f0839c4fb9ad7
Lauri Niva <niva.lauri@gmail.com>
2022-11-07 08:41:36 +0200

    Keskitys

--- src/components/Management/Management.js
+++ src/components/Management/Management.js
@@ -1,4 +1,4 @@
-import { Button, Container, Title } from '@mantine/core'
+import { Button, Container, Group, Title } from '@mantine/core'
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
@@ -15,11 +15,14 @@ function Management() {
 
   return (
     <Container>
-      <Title>
+      <Title my={'lg'} order={2} align={'center'}>
         Hallinta
       </Title>
+      <Group position='center' spacing={'xl'}>
+
       <Link to='/hallinta/kayttajat' > <Button>Käyttäjät</Button></Link>
       <Link to='/hallinta/osastot' > <Button>Osastot</Button></Link>
+      </Group>
     </Container>
   )
 }
