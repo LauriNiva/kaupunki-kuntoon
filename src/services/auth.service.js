5f1447c9d09e86b3e3130a60d56079642db9ee29
Lauri Niva <niva.lauri@gmail.com>
2022-11-09 13:51:23 +0200

    Prettier ja emailredirect lisäys

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,11 +1,11 @@
-import { supabase } from "../supabaseClient";
+import { supabase } from '../supabaseClient';
 
 export const signInWithMagiclink = async (email) => {
-  const { user, session , error } = await supabase.auth.signInWithOtp({
-    email
+  const { user, session, error } = await supabase.auth.signInWithOtp({
+    email,
   });
-  if(session) return session;
-}
+  if (session) return session;
+};
 
 export const signInWithEmailAndPassword = async (email, password) => {
   const { user, session, error } = await supabase.auth.signInWithPassword({
@@ -13,20 +13,24 @@ export const signInWithEmailAndPassword = async (email, password) => {
     password,
   });
   if (session) return session;
-
-}
+};
 
 export const signUpNewUser = async (email, password) => {
   console.log(password);
-  const { data, error } = await supabase.auth.signUp({
-    email,
-    password,
-  });
-  console.log(data)
+  const { data, error } = await supabase.auth.signUp(
+    {
+      email,
+      password,
+    },
+    {
+      emailRedirectTo: window.location.href.replace('signup', 'userprofile'),
+    }
+  );
+  console.log(data);
   console.log(error);
-  if(data)return true;
-}
+  if (data) return true;
+};
 
 export const signOut = async () => {
   const { error } = await supabase.auth.signOut();
-}
+};
